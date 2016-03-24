a778f91cd0298d3f827743efac726e4559726fe1
LonelyTown <710537942@qq.com>
2016-03-24 14:26:20 +0800

    添加了TopView.h文件

---
+++ TopView.h
@@ -0,0 +1,35 @@
+//
+//  TopView.h
+//  demoVc
+//
+//  Created by mac on 15/9/29.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class TopView;
+
+@protocol TopViewDelegate <NSObject>
+
+- (void)TopView:(TopView *)topView ButtonClick:(UIButton *)btn;
+
+@end
+
+
+@interface TopView : UIView
+
+/**
+ *  选项个数
+ */
+@property (nonatomic, assign) NSInteger count;
+
+/**
+ *  选项title数组
+ */
+@property (nonatomic, strong) NSArray *titleArray;
+
+@property (nonatomic, weak) id<TopViewDelegate> delegate;
+
+- (void)btnClickWithIndex:(NSInteger)index;
+
+@end
